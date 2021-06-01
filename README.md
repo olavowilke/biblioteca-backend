396e0e2f2a4fa48621d66337311db17a7fdf893d
Olavo Filho <olavo.wilke@gmail.com>
2021-06-01 09:26:06 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BibliotecaRepository
+ 
---
+++ src/main/java/br/com/biblioteca/AntonioNunesApplication.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntonioNunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntonioNunesApplication.class, args);
+	}
+
+}
